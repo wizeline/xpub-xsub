919a35ec76fa0477f1de9ba2ac50ababc8897bb2
juanpabs <juanpablo.franco@wizeline.com>
2022-02-14 15:29:06 -0600

    Send messages via sdtin to the azure kubernetes cluster

--- publisher/Program.cs
+++ publisher/Program.cs
@@ -1,27 +1,21 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 using NetMQ;
 using NetMQ.Sockets;
-using (var pubSocket = new PublisherSocket(">tcp://0.0.0.0:50552"))
+using (var pubSocket = new PublisherSocket(">tcp://52.226.202.234:50552"))
 {
     Console.WriteLine("Publisher socket connecting...");
     pubSocket.Options.SendHighWatermark = 1000;
-    var rand = new Random(50);
 
     while (true)
     {
-        var randomizedTopic = rand.NextDouble();
-        if (randomizedTopic > 0.5)
-        {
-            var msg = "TopicA msg-" + randomizedTopic;
-            Console.WriteLine("Sending message : {0}", msg);
-            pubSocket.SendMoreFrame("TopicA").SendFrame(msg);
-        }
-        else
-        {
-            var msg = "TopicB msg-" + randomizedTopic;
-            Console.WriteLine("Sending message : {0}", msg);
-            pubSocket.SendMoreFrame("TopicB").SendFrame(msg);
-        }
+        TextReader tIn = Console.In;
+        TextWriter tOut = Console.Out;
+        tOut.Write("Escribe tu mensaje: ");
+        String msg = tIn.ReadLine();
+        Console.WriteLine("Sending message : {0}", msg);
+        pubSocket.SendMoreFrame("TopicB").SendFrame(msg);
+        
     }
 }
